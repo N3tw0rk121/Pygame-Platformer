91b6afc164f0c4b4b0af6129848310c8f68a4924
salli20 <78070458+salli20@users.noreply.github.com>
2021-09-12 08:26:32 +1000

    Bob

--- game.py
+++ game.py
@@ -2,6 +2,10 @@ import pygame, sys
 from pygame.locals import *
 pygame.init() #initiates Pygame
 
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("PyPlatformer")
+
 WINDOWS_SIZE = (720, 1080)
 
 screen = pygame.display.set_mode(WINDOWS_SIZE,0,32)
@@ -9,8 +13,9 @@ screen = pygame.display.set_mode(WINDOWS_SIZE,0,32)
 while True: #Loop
 
     for event in pygame.event.get():
-        if event.type == QUIT
+        if event.type == QUIT:
             pygame.quit
             sys.exit()
 
     pygame.display.update()
+    clock.tick(60)
