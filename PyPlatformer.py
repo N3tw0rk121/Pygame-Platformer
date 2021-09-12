5bc52afe8863a48c46f9dde0a76c798ff004d299
salli20 <78070458+salli20@users.noreply.github.com>
2021-09-12 17:22:21 +1000

    changed character scale, added move, added BG fill

--- PyPlatformer.py
+++ PyPlatformer.py
@@ -10,18 +10,38 @@ WINDOWS_SIZE = (400, 400)
 
 screen = pygame.display.set_mode(WINDOWS_SIZE,0,32)
 
-player_image = pygame.image.load ("assets\clown_main.png")
+player_image = pygame.image.load ("assets\clown_main-300%.png")
 
+moving_right = False
+moving_left = False
 
+player_location = [50,50]
 
 while True: #Loop
+    screen.fill((146,244,255))
 
-    screen.blit(player_image,(50,50))
+    screen.blit(player_image,player_location)
+    
+    if moving_right == True:
+        player_location[0] += 4
+    if moving_left == True:
+        player_location[0] -= 4
+
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit
             sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                moving_right = True
+            if event.key == K_LEFT:
+                moving_left = True
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
 
     pygame.display.update()
     clock.tick(40)
